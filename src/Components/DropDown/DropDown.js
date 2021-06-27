1af523c42bc235996f285dcabf7ee037b0a0eac8
Israt <isratjahanruna6@gmail.com>
2021-06-27 04:52:30 -0700

    done

--- src/Components/DropDown/DropDown.js
+++ src/Components/DropDown/DropDown.js
@@ -3,12 +3,12 @@ import React, { Component, useEffect, useState } from 'react';
 
 
 const DropDown = () => {
-    const [options, setOptions] = useState()
+    const [options, setOptions] = useState([])
     useEffect( () => {
         fetch('http://127.0.0.1:8000/api/dropDown')
         .then(res => res.json())
-        .then(data =>console.log(data))
-    })
+        .then(data =>setOptions(data))
+    },[])
             
            
     return (
@@ -17,10 +17,12 @@ const DropDown = () => {
         <div class="container p-5">
             
                 <select class="custom-slect">
-                {/* {options.map(option =>
-                <option value={option}>{option.products}</option>
-)} */}
+                    <option selected disable>Select A T-Shirt</option>
+                {options.map(option =>
+                <option value={option.id}>{option.products}</option>
+)}
                 </select>
+
 
         
                 
